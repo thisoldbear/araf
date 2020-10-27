5d910eaabe10f369995d9ef5db9a6dc9b612bb7d
thisoldbear <danielwilliams.design@gmail.com>
2020-10-27 22:09:47 +0000

    Add initial Firebase setup

---
+++ firebase/index.js
@@ -0,0 +1,11 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+export default admin;
