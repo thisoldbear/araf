a10d71d58416aedc643d103ff33cd8d9b3b08980
thisoldbear <danielwilliams.design@gmail.com>
2020-11-20 11:16:05 +0000

    Add service account JSON generation

--- utils/auth/firebaseAdmin.ts
+++ utils/auth/firebaseAdmin.ts
@@ -1,10 +1,10 @@
 import * as firebaseAdmin from "firebase-admin";
 
+const serviceAccount = require("../../serviceAccount.json");
+
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(
-      JSON.parse(process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY)
-    ),
+    credential: firebaseAdmin.credential.cert(serviceAccount),
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_BUCKET_URL,
   });
