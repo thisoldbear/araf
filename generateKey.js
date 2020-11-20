45975e6c7d62cfeca874633d9ffb992328fa0941
thisoldbear <danielwilliams.design@gmail.com>
2020-11-20 11:48:47 +0000

    Parse JSON from env var for Vercel

--- generateKey.js
+++ generateKey.js
@@ -5,7 +5,6 @@ const serviceAccount = {
   type: "service_account",
   project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   private_key_id: process.env.FB_KEY_ID,
-  private_key: process.env.FB_KEY,
   client_email: process.env.FB_CLIENT_EMAIL,
   client_id: process.env.FB_CLIENT_ID,
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
@@ -14,6 +13,10 @@ const serviceAccount = {
   client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-rwk7t%40${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.iam.gserviceaccount.com`
 }
 
-fs.writeFile('serviceAccount.json', JSON.stringify(serviceAccount), function (err) {
-  if (err) throw err;
-});
+fs.writeFile('serviceAccount.json', JSON.stringify(
+  {
+    ...serviceAccount,
+    ...JSON.parse(process.env.FB_KEY) // Parse JSON from Vercel env var
+  }), function (err) {
+    if (err) throw err;
+  });
